5b8b74d83e4e81b021014517bef24f42dcbe6160
yoan dorny <y.n.dorny@gmail.com>
2020-04-12 11:53:56 +0200

    Updating

--- mongodb.js
+++ mongodb.js
@@ -11,31 +11,31 @@ MongoClient.connect(connectionUrl, { useUnifiedTopology: true}, (error, client) 
   console.log('Connected 🤖')
   const db = client.db(databaseName)
 
-  //find one
-  // db.collection('users').findOne({ _id: new ObjectID('5e903ac70eab45dfa1142723')}, (error, result) => {
-  //     if(error) return console.log('Unable to fetch ')
-  //     console.log(result)
+  // //find last tasks by id
+  // db.collection('tasks').findOne({ _id: new ObjectID('5e90339a4fbc6d9fa5d2ae63')}, (error, task) => {
+  //   if(error) return console.log('Something went wrong')
+  //   console.log(task)
+  // })
+  // //find all completed tasks
+  // db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+  //   if (error) return console.log('Something went wrong')
+  //   console.log(tasks)
+  // })
+
+  // db.collection('users').updateOne({ _id: new ObjectID('5e902f7fd02ae87c3639842c')}, {
+  //   // $set: {
+  //   //   name: 'Yamcha'
+  //   // }
+  //   $inc:{
+  //     age: 3
   //   }
-  // )
+  // }).then((result) => console.log('🥳 success:', result)).catch((error) => console.log(error))
 
-  // render an array of result
-  // db.collection('users').find({ age: 30 }).toArray((error, users) => {
-  //   console.log(users)
-  // })
-  // render the number of results
-  // db.collection('users').find({ age: 30 }).count((error, count) => {
-  //   console.log(count)
-  // })
+  db.collection('tasks').updateMany({}, {
+    $set:{
+      completed: true
+    }
+  }).then((result) => console.log('🥳 success:', result)).catch((error) => console.log(error))
 
-  //find last tasks by id
-  db.collection('tasks').findOne({ _id: new ObjectID('5e90339a4fbc6d9fa5d2ae63')}, (error, task) => {
-    if(error) return console.log('Something went wrong')
-    console.log(task)
-  })
-  //find all completed tasks
-  db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
-    if (error) return console.log('Something went wrong')
-    console.log(tasks)
-  })
 })
 
