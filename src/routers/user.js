50a69252c1ae35e73e54cf5ec4f1d4fe865657a6
Yoan <y.n.dorny@gmail.com>
2020-04-14 19:12:13 +0200

    Authenticating User Endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -64,8 +64,7 @@ router.get('/users/:id', async (req, res) => {
   }
 })
 
-router.patch('/users/:id', async (req, res) => {
-  const _id = req.params.id
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['age', 'email', 'name', 'password']
   const isValid = updates.every((update) => allowedUpdates.includes(update))
@@ -73,8 +72,8 @@ router.patch('/users/:id', async (req, res) => {
   if (!isValid) return res.status(400).send({ error: 'Invalid updates!' })
 
   try {
-    const user = await User.findById(_id)
-    updates.forEach((update) =>  user[update] = req.body[update])
+    const {user, body} = req
+    updates.forEach((update) =>  user[update] = body[update])
     await user.save()
 
     if (!user) return res.status(404).send()
@@ -84,13 +83,10 @@ router.patch('/users/:id', async (req, res) => {
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
-  const _id = req.params.id
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(_id)
-    if (!user) return res.status(404).send()
-    res.send(user)
-
+    await req.user.remove()
+    res.send(req.user)
   } catch (e) { res.status(500).send(e) }
 })
 
