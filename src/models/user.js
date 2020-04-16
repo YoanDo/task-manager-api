983bf18e2f6ec2b7c800f778ad9f058a6e92ab70
Yoan <y.n.dorny@gmail.com>
2020-04-16 12:37:33 +0200

    Time stamp

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose');
+
+
+const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,42 +12,46 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"')
+      }
+    }
+  },
   age: {
     type: Number,
     default: 0,
     validate(value) {
       if (value < 0) {
-        throw new Error('Age must be a positive number')
+        throw new Error('Age must be a postive number')
       }
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Invalid email')
-      }
-    }
-  },
-  password: {
-    minlength: 7,
-    required: true,
-    trim: true,
-    type: String,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) throw new Error("'password' is a super dumb password, pick another one ")
     }
   },
   tokens: [{
-    token:{
+    token: {
       type: String,
-      required: true,
+      required: true
     }
   }]
+}, {
+  timestamps: true
 })
 
 userSchema.virtual('tasks', {
@@ -53,39 +60,58 @@ userSchema.virtual('tasks', {
   foreignField: 'creator' //field name on the other side that will receive the value
 })
 
-
-userSchema.methods.generateAuthToken = async function () {
-  const user = this
-  const token = jwt.sign({_id: user._id.toString()}, 'thisisatest')
-
-  user.tokens = user.tokens.concat({token})
-  await user.save()
-  return token
-}
-
-// method to hide sensitive data
-userSchema.methods.toJSON = function() {
+// here we hide sensitive data
+userSchema.methods.toJSON = function () {
   const user = this
   const userObject = user.toObject()
 
   delete userObject.password
   delete userObject.tokens
+
   return userObject
 }
 
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisisatest')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({email: email})
-  if (!user) throw newError('Unable to login')
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
   const isMatch = await bcrypt.compare(password, user.password)
-  if (!isMatch) throw new Error('Unable to login')
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
 
   return user
 }
 
-//hash the plain text password before saving
-userSchema.pre('save', async function(next){ // mean it will be run before 'pre' the saving
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
   const user = this
-  if(user.isModified('password')) user.password = await bcrypt.hash(user.password, 8) //encrypt password
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this
+  await Task.deleteMany({ creator: user._id })
   next()
 })
 
